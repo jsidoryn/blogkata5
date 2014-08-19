8b729b05ae9094ba3990c938e01abfab5fd70e6f
Jason Sidoryn <jason@katalyst.com.au>
2014-08-20 08:34:46 +0930

    for comments match on the object rather than the id

--- app/routes/comments.js
+++ app/routes/comments.js
@@ -5,7 +5,7 @@ export default Ember.Route.extend({
 		var self = this;
 		return this.store.find('comment').then(function(comments) {
 			return comments.filter(function(comment) {
-				return comment.get('post.id') === self.modelFor('post').get('id');
+				return comment.get('post') === self.modelFor('post');
 			});
 		});
 	}
